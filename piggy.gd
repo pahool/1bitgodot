08af9c3102a4ed988a6bd0228f2c7c1b10941c4f
Paul Sitko <git_bo@pahool.com>
2023-11-21 12:23:20 -0800

    adds run cycle

--- piggy.gd
+++ piggy.gd
@@ -2,7 +2,19 @@ extends Area2D
 
 @export var speed = 100
 
+@onready var animation_player = $AnimationPlayer
+
+@onready var sprite_2d = $Sprite2D
+
 func _process(delta):
-	
 	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if input_vector == Vector2.ZERO:
+		animation_player.play("idle")
+	else:
+		animation_player.play("Run")
+		if input_vector.x > 0:
+			sprite_2d.scale.x = 1
+		elif input_vector.x < 0:
+			sprite_2d.scale.x = -1
+			
 	position += input_vector * speed * delta
