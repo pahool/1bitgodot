16a1779435f8756cd9765f9f7cea22dfd2225cf1
Paul Sitko <git_bo@pahool.com>
2023-11-21 11:51:13 -0800

    adds delta and export variable

--- piggy.gd
+++ piggy.gd
@@ -1,9 +1,8 @@
 extends Area2D
 
-var speed = 2
+@export var speed = 100
 
 func _process(delta):
 	
 	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	position += input_vector * speed
-
+	position += input_vector * speed * delta
