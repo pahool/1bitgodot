231a446c506a722a4a816b73b1cffdf84fe1d6bc
Paul Sitko <git_bo@pahool.com>
2023-12-02 08:04:33 -0800

    before rename

--- Space Shooter/ship.gd
+++ Space Shooter/ship.gd
@@ -9,7 +9,7 @@ func _process(delta):
 		position.y -= speed * delta
 	if Input.is_action_pressed("ui_down"):
 		position.y += speed * delta
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_pressed("ui_accept"):
 		var laser = LASER_SCENE.instantiate()
 		var world = get_tree().current_scene
 		world.add_child(laser)
