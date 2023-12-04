cd95082554cef45659527d3f2d28bc46c262ab59
Paul Sitko <git_bo@pahool.com>
2023-12-04 11:52:24 -0800

    adds sound and end screen

--- Space Shooter/world.gd
+++ Space Shooter/world.gd
@@ -9,3 +9,8 @@ var score = 0:
 		score = value
 		score_label.text = "score: " + str(score)
 		
+
+
+func _on_ship_ship_destroyed():
+	await get_tree().create_timer(2.0).timeout
+	get_tree().change_scene_to_file("res://GameOverScreen.tscn")
