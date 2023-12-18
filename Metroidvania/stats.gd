87102b87aac760c44503ed42e2f98f52489f776a
Paul Sitko <git_bo@pahool.com>
2023-12-18 11:06:18 -0800

    end of lesson 16

---
+++ Metroidvania/stats.gd
@@ -0,0 +1,14 @@
+extends Node
+
+@export var max_health = 3 : set = set_max_health
+@onready var health = max_health : set = set_health
+
+signal no_health
+
+func set_max_health(value):
+	max_health = value 
+
+func set_health(value):
+	health = value
+	if health <= 0: no_health.emit()
+	
