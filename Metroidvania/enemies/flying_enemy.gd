d023e27ba0e58e80c065291ebc966aca3d0bb3c6
Paul Sitko <git_bo@pahool.com>
2024-01-05 14:11:43 -0800

    end of lesson 30 - for real

--- Metroidvania/enemies/flying_enemy.gd
+++ Metroidvania/enemies/flying_enemy.gd
@@ -7,8 +7,10 @@ const EnemyDeathEffectScene = preload("res://effects/enemy_death_effect.tscn")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var stats = $Stats as Stats
-@onready var waypoint_pathfinding = $VisibleOnScreenEnabler2D/WaypointPathfinding 
+@onready var waypoint_pathfinding = $WaypointPathfinding
 
+func _ready():
+	set_physics_process(false)
 
 func _physics_process(delta):
 	var player = MainInstances.player
@@ -27,3 +29,7 @@ func _on_hurt_box_hurt(hitbox, damage):
 func _on_stats_no_health():
 	queue_free()
 	Utils.instantiate_scene_on_world(EnemyDeathEffectScene, global_position)
+
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	set_physics_process(true)
+	
