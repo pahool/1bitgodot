ecc41c0638213382594b95ec3a7e5421a07fbf20
Paul Sitko <git_bo@pahool.com>
2024-01-11 12:18:09 -0800

    end of lesson 38

---
+++ Metroidvania/world/door.gd
@@ -0,0 +1,23 @@
+class_name Door
+extends Area2D
+
+@export_file("*.tscn") var new_level_path
+
+@export var connection : DoorConnection
+
+@onready var right_cast = $RightCast
+@onready var left_cast = $LeftCast
+
+func get_diretion():
+	if left_cast.is_colliding(): return -1
+	if right_cast.is_colliding(): return 1
+	return 0
+
+func _physics_process(delta):
+	var player = MainInstances.player as Player
+	if not player is Player: return
+	if overlaps_body(player) and new_level_path :
+		var player_direction = sign(player.velocity.x)
+		var direction = get_diretion()
+		if player_direction == direction:
+			Events.door_entered.emit(self)
---
+++ Metroidvania/world/door_connection.gd
@@ -0,0 +1,3 @@
+class_name DoorConnection
+extends Resource
+
