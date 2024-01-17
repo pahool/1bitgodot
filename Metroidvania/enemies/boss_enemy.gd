2e11c8a221f8d2f37d06b670820f6cf0a4712d44
Paul Sitko <git_bo@pahool.com>
2024-01-17 12:10:26 -0800

    end of lesson 44

--- Metroidvania/enemies/boss_enemy.gd
+++ Metroidvania/enemies/boss_enemy.gd
@@ -1,20 +1,40 @@
 extends Node2D
 
 const StingerScene = preload("res://enemies/stinger.tscn")
+const MissilePowerUpScene = preload("res://player/missile_powerup.tscn")
+const EnemyDeathEffectScene = preload("res://effects/enemy_death_effect.tscn")
 
 @export var acceleration =200
 @export var max_speed = 800
 @export var targets : Array[NodePath]
 
-var state = rush_state
+var state = recenter_state : set = set_state
+var state_init = true : get = get_state_init
 var velocity = Vector2.ZERO
+
+var STATE_OPTIONS = [fire_state, fire_state, rush_state, rush_state]
+var state_options_left = []
 
 
 @onready var stats = $Stats
 @onready var stinger_pivot = $StingerPivot
 @onready var muzzle = $StingerPivot/Muzzle
 @onready var firerate_timer = $FirerateTimer
+@onready var state_timer = $StateTimer
 
+func set_state(value):
+	state = value
+	state_init = true 
+	
+func get_state_init():
+	var was = state_init
+	state_init = false
+	return was
+	
+func _ready():
+	var freed = WorldStash.retrieve("first_boss", "freed")
+	if freed: queue_free()
+	
 func _process(delta):
 	state.call(delta)
 
@@ -22,6 +42,9 @@ func _on_hurtbox_hurt(hitbox, damage):
 	stats.health -= damage
 
 func rush_state(delta):
+	if state_init:
+		state_timer.start(randf_range(4.0, 6.0))
+		state_timer.timeout.connect(set_state.bind(decelerate_state), CONNECT_ONE_SHOT)
 	var player = MainInstances.player
 	if not player is Player: return
 	var direction = global_position.direction_to(player.global_position) 
@@ -29,6 +52,9 @@ func rush_state(delta):
 	move(delta)
 	
 func fire_state(delta):
+	if state_init:
+		state_timer.start(randf_range(4.0, 6.0))
+		state_timer.timeout.connect(set_state.bind(recenter_state), CONNECT_ONE_SHOT)
 	if firerate_timer.time_left == 0:
 		stinger_pivot.rotation_degrees += 17
 		firerate_timer.start()
@@ -38,13 +64,17 @@ func fire_state(delta):
 
 func recenter_state(delta):
 	assert(not targets.is_empty())
-	set_process(false)
-	var center_node = get_node(targets.pick_random())
-	var tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(self, "global_position", center_node.global_position, 2.0)
-	await tween.finished
-	set_process(true)
-	state = rush_state(delta)
+	if state_init:
+		var center_node = get_node(targets.pick_random())
+		var tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+		tween.tween_property(self, "global_position", center_node.global_position, 2.0)
+		await tween.finished
+		state_timer.start(0.5)
+		await state_timer.timeout
+		if state_options_left.is_empty():
+			state_options_left = STATE_OPTIONS.duplicate()
+			state_options_left.shuffle()
+		state = state_options_left.pop_back()
 	
 func decelerate_state(delta):
 	velocity = velocity.move_toward(Vector2.ZERO, acceleration * delta)
@@ -57,6 +87,7 @@ func move(delta):
 	
 func _on_stats_no_health():
 	queue_free()
-
-func _on_state_timer_timeout():
-	state = decelerate_state
+	WorldStash.stash("first_boss", "freed", "true")
+	Utils.instantiate_scene_on_level(MissilePowerUpScene, global_position)
+	for i in 10:
+		Utils.instantiate_scene_on_level(EnemyDeathEffectScene, global_position+Vector2(randf_range(-16, 16), randf_range(-16, 16)))
